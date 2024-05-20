073008d5a1ed2befa2a30a456f9edff03f609afc
Elvis2280 <Elvismiranda924g@gmail.com>
2024-05-21 01:43:03 +0900

    add transition when page load

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
   prefix: "",
-  darkMode: "selector",
   theme: {
     container: {
       center: true,
@@ -12,46 +17,6 @@ module.exports = {
       },
     },
     extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -92,14 +57,52 @@ module.exports = {
             height: "100%",
           },
         },
+        transitionBg:{
+          "0%": {
+            background: "black",
+            zIndex: 99999,
+          },
+          "50%": {
+            background: "black",
+
+          },
+            "60%": {
+                background: "transparent",
+
+            },
+          "100%": {
+            background: "transparent",
+
+          },
+        },
+        transitionBar:{
+          "0%": {
+            transform: "translateX(-100%)",
+          },
+          "50%": {
+            transform: "translateX(0%)",
+          },
+          // "50%": {
+          //   transform: "translateX(50%)",
+          // },
+          // "60%": {
+          //   transform: "translateX(50%)",
+          // },
+          "100%": {
+            transform: "translateX(100%)",
+
+          },
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         bg: "bg 7s infinite",
         navbarBar: "navbarBar 2s ease-in-out",
+        transitionBg: "transitionBg 2s ease-in-out",
+        barTransition: "transitionBar 2s ease-in-out"
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-};
+}
