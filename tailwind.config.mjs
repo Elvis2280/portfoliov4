9f0e9485a4ace4133fc923037dd8ba2a066f8402
Elvis2280 <Elvismiranda924g@gmail.com>
2024-05-22 16:17:41 +0900

    added backend icons and icons animations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -19,12 +19,12 @@ module.exports = {
     extend: {
       keyframes: {
         "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+          from: {height: "0"},
+          to: {height: "var(--radix-accordion-content-height)"},
         },
         "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+          from: {height: "var(--radix-accordion-content-height)"},
+          to: {height: "0"},
         },
         bg: {
           "0%": {
@@ -57,7 +57,7 @@ module.exports = {
             height: "100%",
           },
         },
-        transitionBg:{
+        transitionBg: {
           "0%": {
             background: "black",
             zIndex: 99999,
@@ -66,42 +66,163 @@ module.exports = {
             background: "black",
 
           },
-            "60%": {
-                background: "transparent",
+          "60%": {
+            background: "transparent",
 
-            },
+          },
           "100%": {
             background: "transparent",
 
           },
         },
-        transitionBar:{
+        transitionBar: {
           "0%": {
             transform: "translateX(-100%)",
           },
           "50%": {
             transform: "translateX(0%)",
           },
-          // "50%": {
-          //   transform: "translateX(50%)",
-          // },
-          // "60%": {
-          //   transform: "translateX(50%)",
-          // },
           "100%": {
             transform: "translateX(100%)",
 
           },
-        }
+        },
+        nodeIcon: {
+          "0%": {
+            transform: "translate(0,0)",
+            opacity: 0,
+          },
+
+          "100%": {
+            transform: "translate(158px, -256px)",
+            opacity: 1,
+          },
+        },
+        goIcon: {
+          "0%": {
+            transform: "translate(0, 0)",
+            opacity: 0,
+          },
+
+          "100%": {
+            transform: "translate(206px, -176px)",
+            opacity: 1,
+          },
+        },
+        pythonIcon: {
+          "0%": {
+            transform: "translate(0, 0)",
+            opacity: 0,
+          },
+
+          "100%": {
+            transform: "translate(238px, -80px)",
+            opacity: 1,
+          },
+        },
+        dockerIcon: {
+          "0%": {
+            transform: "translate(0, 0)",
+            opacity: 0,
+          },
+
+          "100%": {
+            transform: "translate(270px, -240px)",
+            opacity: 1,
+          },
+        },
+        dotNetIcon: {
+          "0%": {
+            transform: "translate(0, 0)",
+            opacity: 0,
+          },
+
+          "100%": {
+            transform: "translate(315px, -135px)",
+            opacity: 1,
+          },
+        },
+        tailwindIcon: {
+          "0%": {
+            transform: "translate(0,0)",
+            opacity: 0,
+          },
+
+          "100%": {
+            transform: "translate(-365px, -135px)",
+            opacity: 1,
+          },
+        },
+        nextIcon: {
+          "0%": {
+            transform: "translate(0, 0)",
+            opacity: 0,
+          },
+
+          "100%": {
+            transform: "translate(-288px, -80px)",
+            opacity: 1,
+          },
+        },
+        astroIcon: {
+          "0%": {
+            transform: "translate(0, 0)",
+            opacity: 0,
+          },
+
+          "100%": {
+            transform: "translate(-256px, -176px)",
+            opacity: 1,
+          },
+        },
+        reactIcon: {
+          "0%": {
+            transform: "translate(0, 0)",
+            opacity: 0,
+          },
+
+          "100%": {
+            transform: "translate(-208px, -256px)",
+            opacity: 1,
+          },
+        },
+        jsIcon: {
+          "0%": {
+            transform: "translate(0, 0)",
+            opacity: 0,
+          },
+
+          "100%": {
+            transform: "translate(-320px, -240px)",
+            opacity: 1,
+          },
+        },
       },
+
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         bg: "bg 7s infinite",
         navbarBar: "navbarBar 2s ease-in-out",
         transitionBg: "transitionBg 2s ease-in-out",
-        barTransition: "transitionBar 2s ease-in-out"
+        barTransition: "transitionBar 2s ease-in-out",
+        tailwindIcon: "tailwindIcon 2.6s ease-in-out",
+        nextIcon: "nextIcon 2.8s ease-in-out",
+        astroIcon: "astroIcon 2.4s ease-in-out",
+        reactIcon: "reactIcon 2s ease-in-out",
+        jsIcon: "jsIcon 2.2s ease-in-out",
+        nodeIcon: "nodeIcon 2.6s ease-in-out",
+        goIcon: "goIcon 2.8s ease-in-out",
+        pythonIcon: "pythonIcon 2.4s ease-in-out",
+        dockerIcon: "dockerIcon 2.2s ease-in-out",
+        dotNetIcon: "dotNetIcon 2.6s ease-in-out",
       },
+      animationDelay: {
+        '2': '2s',
+        '3': '3s',
+        '4': '4s',
+        '5': '5s',
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
